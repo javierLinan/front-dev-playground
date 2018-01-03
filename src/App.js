9d163935322d82ef30d301d7cae243bc805cc0de
Jose Javier Linan Manzaneda <javier.linmanzaneda@gmail.com>
2018-01-03 09:57:22 +0100

    Remove code with bugs
    t005

--- src/App.js
+++ src/App.js
@@ -4,55 +4,6 @@ import './App.css';
 
 class App extends Component {
   render() {
-    const test = a;
-
-    test = 2
-
-    let target =-5;
-    let num = 3;
-
-    target =- num;  // Noncompliant; target = -3. Is that really what's meant?
-    target =+ num; // Noncompliant; target = 3
-
-    var a = NaN;
-
-    if (a === NaN) {  // Noncompliant; always false
-      console.log("a is not a number");  // this is dead code
-    }
-    if (a !== NaN) { // Noncompliant; always true
-      console.log("a is not NaN"); // this statement is not necessarily true
-    }
-
-    function MyClass() {
-      this.foo = 'bar';
-    }
-
-    var someClass = 1;
-
-    var obj1 = new someClass;    // Noncompliant;
-    var obj2 = new MyClass();    // Noncompliant if considerJSDoc parameter set to true. Compliant when considerJSDoc=false
-
-    var a = NaN;
-
-    if (a === NaN) {  // Noncompliant; always false
-      console.log("a is not a number");  // this is dead code
-    }
-    if (a !== NaN) { // Noncompliant; always true
-      console.log("a is not NaN"); // this statement is not necessarily true
-    }
-
-    var x = 'a';
-
-    var foo = {
-      y: 1
-    }
-
-    with (foo) {  // Noncompliant
-      y = 4;  // updates foo.x
-      x = 3;  // does NOT add a foo.x property; updates x var in outer scope
-    }
-    print(foo.x + " " + x); // shows: undefined 3
-
     return (
       <div className="App">
         <header className="App-header">
