5094c374514282011a4d4412b374873683628ea0
Jose Javier Linan Manzaneda <javier.linmanzaneda@gmail.com>
2018-01-19 10:42:46 +0100

    Wrong code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,30 @@ import './App.css';
 
 class App extends Component {
   render() {
+    function MyClass() {
+      this.foo = 'bar';
+    }
+
+    var someClass = 1;
+
+    var obj1 = new someClass;    // Noncompliant;
+    var obj2 = new MyClass();    // Noncompliant if considerJSDoc parameter set to true. Compliant when considerJSDoc=false
+
+    var a = NaN;
+
+    if (a === NaN) {  // Noncompliant; always false
+      console.log("a is not a number");  // this is dead code
+    }
+    if (a !== NaN) { // Noncompliant; always true
+      console.log("a is not NaN"); // this statement is not necessarily true
+    }
+
+    $have_time = true;
+    $have_money = false;
+    $take_vacation = $have_time and $have_money;  // Noncompliant. $take_vacation == true.
+
+
+
     return (
       <div className="App">
         <header className="App-header">
