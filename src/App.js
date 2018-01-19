a8b8ad32a2204b7dd64ed91bfb1106f2c7f6f849
Jose Javier Linan Manzaneda <javier.linmanzaneda@gmail.com>
2018-01-19 11:05:57 +0100

    Wrong code

--- src/App.js
+++ src/App.js
@@ -26,7 +26,77 @@ class App extends Component {
     $have_money = false;
     $take_vacation = $have_time and $have_money;  // Noncompliant. $take_vacation == true.
 
+    eval = 17; // Noncompliant
+    arguments++; // Noncompliant
+    ++eval; // Noncompliant
+    var obj = { set p(arguments) { } }; // Noncompliant
+    var eval; // Noncompliant
+    try { } catch (arguments) { } // Noncompliant
+    function x(eval) { } // Noncompliant
+    function arguments() { } // Noncompliant
+    var y = function eval() { }; // Noncompliant
+    var f = new Function("arguments", "return 17;"); // Noncompliant
 
+    function fun() {
+      if (arguments.length == 0) { // Compliant
+        // do something
+      }
+    }
+
+    function foo(obj1, obj2, array) {
+      var a = obj1.a;  // Noncompliant
+      var b = obj1.b;
+
+      var name = obj2.name;  // ignored; there's only one extraction-and-assignment
+
+      var zero = array[0];  // Noncompliant
+      var one = array[1];
+    }
+
+    var myArray = ['a', 'b', 'c', 'd'];
+
+    delete myArray[2];  // Noncompliant. myArray => ['a', 'b', undefined, 'd']
+    console.log(myArray[2]); // expected value was 'd' but output is undefined
+
+    var x = "Foo";
+    "length" in x; // Noncompliant: TypeError
+    0 in x;        // Noncompliant: TypeError
+
+    var a = NaN;
+
+    if (a === NaN) {  // Noncompliant; always false
+      console.log("a is not a number");  // this is dead code
+    }
+    if (a !== NaN) { // Noncompliant; always true
+      console.log("a is not NaN"); // this statement is not necessarily true
+    }
+
+    var a = NaN;
+
+    if (a === NaN) {  // Noncompliant; always false
+      console.log("a is not a number");  // this is dead code
+    }
+    if (a !== NaN) { // Noncompliant; always true
+      console.log("a is not NaN"); // this statement is not necessarily true
+    }
+
+    var a = NaN;
+
+    if (a === NaN) {  // Noncompliant; always false
+      console.log("a is not a number");  // this is dead code
+    }
+    if (a !== NaN) { // Noncompliant; always true
+      console.log("a is not NaN"); // this statement is not necessarily true
+    }
+
+    var a = NaN;
+
+    if (a === NaN) {  // Noncompliant; always false
+      console.log("a is not a number");  // this is dead code
+    }
+    if (a !== NaN) { // Noncompliant; always true
+      console.log("a is not NaN"); // this statement is not necessarily true
+    }
 
     return (
       <div className="App">
